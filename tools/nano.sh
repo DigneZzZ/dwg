77379eb03a0c52d28476a13af107747a373e56ea
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-05-01 22:05:57 +0300

    Create nano.sh

---
+++ tools/nano.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Устанавливаем редактор Nano
+if ! command -v nano &> /dev/null
+then
+    read -p "Хотите установить текстовый редактор Nano? (y/n) " INSTALL_NANO
+    if [ "$INSTALL_NANO" == "y" ]; then
+        apt-get update
+        apt-get install -y nano
+    fi
+else
+    echo "Текстовый редактор Nano уже установлен."
+fi
