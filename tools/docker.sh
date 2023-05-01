4e135e2e4bf9d04afe40a73d5649bb1c4c034257
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-05-02 02:56:32 +0300

    Update docker.sh

--- tools/docker.sh
+++ tools/docker.sh
@@ -56,3 +56,4 @@ else
     # Установка docker-compose
     curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose &&
     chmod +x /usr/local/bin/docker-compose
+fi
