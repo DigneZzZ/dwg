de6de7047985d611b2a471ac1fd6b06c2d94f67e
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-06-06 00:41:35 +0300

    Update set-up.sh

--- set-up.sh
+++ set-up.sh
@@ -400,6 +400,23 @@ fi
 
 printf "${GREEN} Этап проверки докера закончен, можно продолжить установку\n${NC}"
 
+  printf "Для корректной работы данной сборки необходимо освободить 53 порт. Сделать это автоматическим скриптом? Гарантий работоспособности на вашей операционной системе мы не даем!!! (Y/n) (по умолчанию - Y, можете нажать Enter): "
+read choice_resolv
+
+  if [[ $choice_resolv == "" || $choice_resolv == "Y" || $choice_resolv == "y" ]]; then
+    sh -c 'echo DNSStubListener=no >> /etc/systemd/resolved.conf' && systemctl restart systemd-resolved.service
+    systemctl stop systemd-resolved.service &&
+    rm -f /etc/resolv.conf &&
+    ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf &&
+    systemctl start systemd-resolved.service
+    printf "\e[42mИмзенения в систему внесены. Но если установка контейнера завершится с ошибок, вам необходимо перезапустить сервер.\e[0m\n"
+    printf "\e[42mВыполнение скрипта будет продолжено в любом случае через 5 секунд\e[0m\n"
+    sleep 5
+  else
+    printf "Скрипт не будет запущен.\n"
+    exit 1
+  fi
+
 # Получаем внешний IP-адрес
 MYHOST_IP=$(hostname -I | cut -d' ' -f1)
 
