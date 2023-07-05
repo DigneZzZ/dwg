319540d177be19658d1e6e10aac4836bd5780a04
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-07-05 10:09:49 +0300

    Update block_torrent.sh

--- tools/block_torrent.sh
+++ tools/block_torrent.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-    sudo ufw deny from any to any proto any string "BitTorrent" comment "Blocking BitTorrent"
-   sudo ufw deny from any to any proto any string "BitTorrent protocol" comment "Blocking BitTorrent protocol"
-   sudo ufw deny from any to any proto any string "peer_id=" comment "Blocking peer_id="
-   sudo ufw deny from any to any proto any string ".torrent" comment "Blocking .torrent"
-   sudo ufw deny from any to any proto any string "announce.php?passkey=" comment "Blocking announce.php?passkey="
-   sudo ufw deny from any to any proto any string "torrent" comment "Blocking torrent"
-   sudo ufw deny from any to any proto any string "announce" comment "Blocking announce"
-   sudo ufw deny from any to any proto any string "info_hash" comment "Blocking info_hash"
+iptables -I FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -I FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -I FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -I FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -I FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -I FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -I FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -I FORWARD -m string --algo bm --string "info_hash" -j DROP
