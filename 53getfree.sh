299d13b5dfedf39fe2bdfdd0b48ca6e5aaadd94a
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-07-16 00:01:19 +0300

    Update 53getfree.sh

--- 53getfree.sh
+++ 53getfree.sh
@@ -4,15 +4,9 @@
 read choice_resolv
 
   if [[ $choice_resolv == "" || $choice_resolv == "Y" || $choice_resolv == "y" ]]; then
-    sh -c 'echo DNSStubListener=no >> /etc/systemd/resolved.conf' && systemctl restart systemd-resolved.service
-    systemctl stop systemd-resolved.service &&
-    rm -f /etc/resolv.conf &&
-    ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf &&
-    systemctl start systemd-resolved.service
-    printf "\e[42mИмзенения в систему внесены. НЕОБХОДИМО ПЕРЕЗАПУСТИТЬ СЕРВЕР! запустите скрипт ./dwg/set-up.sh после перезагрузки.\e[0m\n"
-    printf "\e[42mВыполнение скрипта будет продолжено в любом случае через 5 секунд\e[0m\n"
-    sleep 5
-    reboot
+    echo "DNSStubListener=no" >>/etc/systemd/resolved.conf
+    ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+        printf "Теперь желательно перезапустить контейнер/сервер.. но это не всегда обязательно"
   else
     printf "Скрипт не будет запущен.\n"
     exit 1
